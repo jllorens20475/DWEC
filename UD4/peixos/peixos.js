b3ef90c5df10ae5fb15c663d5532bbee4b1f7d9c
jllorens20475 <jllorens20475@iesjoanramis.org>
2024-10-09 19:11:30 +0200

    Peixos V2 w// Comentarios

--- UD4/peixos/peixos.js
+++ UD4/peixos/peixos.js
@@ -1,19 +1,18 @@
-function comptarPeixos(cadena) {
-    //DEFINIM EL INDEX DE LA NOSTRA CADENA, AQUESTA FUNCIÓ MIRA SI EN LA CADENA HI HA ELS CARACTERS ESPECIFICATS
-    let index = cadena.indexOf("><>")   
+const imatgeSonda = "~~~><>~~~><>><>~~~><>~~~~~~><>~><>~";
 
-        // SI ES -1 NO HI HA PEIXOS
-        if(index === -1){
-            
-            //RETORNAM 0 PERQUE NO HI HA MES PEIXOS
-            return 0;  
-        
-        }else{
-           
-            //GUARDAM 1 PEIX I TORNAM A ENTRAR A LA FUNCIÓ, RESTANT EL PEIX JA GUARDAT
-            return 1 + comptarPeixos(cadena.slice(index + 3))  
-        }
-    }
+function comptarPeixos(cadena) {
+  //DEFINIM EL INDEX DE LA NOSTRA CADENA, AQUESTA FUNCIÓ MIRA SI EN LA CADENA HI HA ELS CARACTERS ESPECIFICATS
+  let index = cadena.indexOf("><>");
+
+  // SI ES -1 NO HI HA PEIXOS
+  if (index === -1) {
+    //RETORNAM 0 PERQUE NO HI HA MES PEIXOS
+    return 0;
+  } else {
+    //GUARDAM 1 PEIX I TORNAM A ENTRAR A LA FUNCIÓ, RESTANT EL PEIX JA GUARDAT
+    return 1 + comptarPeixos(cadena.slice(index + 3));
+  }
+}
 
 //CRIDA A LA FUNCIÖ
-console.log(comptarPeixos("~~><>~~><>~><>~~~~"));
+console.log(comptarPeixos(imatgeSonda));
